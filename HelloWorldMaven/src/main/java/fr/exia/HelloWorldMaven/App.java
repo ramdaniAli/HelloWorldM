ffb433d5c235625df0f45680ce805f92f78e2de9
liabe <ali.ramdani.dz@viacesi.fr>
2019-05-19 23:51:40 +0100

    helloWorldMvc

---
+++ HelloWorldMaven/src/main/java/fr/exia/HelloWorldMaven/App.java
@@ -0,0 +1,13 @@
+package fr.exia.HelloWorldMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
